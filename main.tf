a7e2e13a4ad398344bb6c705d82b9f9fdccb8826
Tingsheng Lai <laitingsheng@hotmail.com>
2025-07-07 22:04:13 +0800

    Add Terraform entrance
    Signed-off-by: Tingsheng Lai <laitingsheng@hotmail.com>

---
+++ main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_version = "~> 1.0"
+
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 3.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "azurerm" {
+    use_azuread_auth = true
+    use_cli          = true
+    environment      = "public"
+  }
+}
+
+provider "azuread" {
+  use_cli     = true
+  environment = "public"
+}
+
+provider "azurerm" {
+  use_cli                         = true
+  environment                     = "public"
+  resource_provider_registrations = "extended"
+  storage_use_azuread             = true
+
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+
+    subscription {
+      prevent_cancellation_on_destroy = false
+    }
+  }
+}
+
+data "azuread_client_config" "main" {}
+
+data "azurerm_client_config" "main" {}
+
+data "azurerm_subscription" "main" {
+  subscription_id = data.azurerm_client_config.main.subscription_id
+}
+
+resource "azurerm_resource_group" "connectivity" {
+  name     = "rg-connectivity-ea"
+  location = "eastasia"
+}
+
+resource "azurerm_resource_group" "tfstate" {
+  name     = "rg-tfstate-ea"
+  location = "eastasia"
+}
