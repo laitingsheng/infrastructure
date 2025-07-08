c6569ff18d8db17471fb7577e3f556cce29e7b07
Tingsheng Lai <laitingsheng@hotmail.com>
2025-07-08 10:51:36 +0800

    Assign permission for subscription alias
    Signed-off-by: Tingsheng Lai <laitingsheng@hotmail.com>

--- rbac.tf
+++ rbac.tf
@@ -1,3 +1,10 @@
+resource "azurerm_role_assignment" "alias-main" {
+  scope                = azurerm_subscription.main.id
+  role_definition_name = "Contributor"
+  principal_id         = azuread_service_principal.devops.object_id
+  principal_type       = "ServicePrincipal"
+}
+
 resource "azurerm_role_assignment" "subscription-main" {
   for_each = toset([
     "Contributor",
